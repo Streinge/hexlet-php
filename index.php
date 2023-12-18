d4292c5b802a1911d86c82fc074aa3ea1ae7972f
OlegSofonov <olesof@mail.ru>
2023-12-18 12:39:17 +0500

    work 18/12 12:39

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 // BEGIN
 function  isHappy(string $number): bool
 {   
@@ -14,3 +15,19 @@ function  isHappy(string $number): bool
     return $sumBegin === $sumEnd;
 }
 var_dump(isHappy('00'));
+=======
+function joinNumbersFromRange($number1, $number2)
+{
+    // BEGIN (write your solution here)
+    $i = $number1;
+    $result = '';
+    while ($i <= $number2) {
+        $result = "{$result}{$i}";
+        $i = $i + 1;
+    }
+    return $result;
+    // END
+}
+// Вызов функции 
+print_r(joinNumbersFromRange(4, 10));
+>>>>>>> Stashed changes
