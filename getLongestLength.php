a9e97439e716dee87d0d113fd8330b89786e2d31
OlegSofonov <olesof@mail.ru>
2023-12-22 06:23:22 +0500

    home 22/12 6:23

--- getLongestLength.php
+++ getLongestLength.php
@@ -1,13 +1,32 @@
 <?php
 
 namespace Hexlet\Php\GetLongestLength;
-function 
 
-function getLongestLength(string $str): int
+function findsSubstring(string $str)
 {
-    $number = 0;
-    substr = '';
-    for ()
-    return $number;
-
+    if (strlen($str) === 1) {
+        return $str;
+    }
+    $firstChar = $str[0];
+    $substr = $firstChar;
+    $size = strlen($str);
+    for ($i = 1; $i < $size; $i++) {
+        if ($str[$i] === $firstChar) {
+            return findsSubstring(substr($substr, 1));
+        } else {
+            $substr .= "{$str[$i]}";
+        }
+    }
+    return $substr;
 }
+
+function getLongestLength(string $str)
+{
+    $char = $str[0];
+    $substr = findsSubstring($str);
+
+    return $substr;
+}
+
+$str = 'qweqrty';
+echo getLongestLength($str) . "\n";
--- primer.php
+++ primer.php
@@ -3,10 +3,9 @@
 
 // foreach ($array1 as $b) echo json_encode($b),"\n";
 
-$result = 'qweqrty';
-$new = substr_count($result, 'q');
-echo $new;
-
+$str = 'qweqrty';
+$slice = substr($str, 1);
+echo $slice . "\n";
 
 
 
