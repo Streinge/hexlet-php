1dd33419a6ac0abc179de3898e788d6317502959
OlegSofonov <olesof@mail.ru>
2023-12-25 00:31:07 +0500

    home 25/12 0:30

--- primer.php
+++ primer.php
@@ -1,17 +1,22 @@
 <?php
 
-$array1 = [1, 2, 3, 4, 5];
-$array2 = [7, 8, 9, 10, 11, 12];
-$array3 = [3, 4, 5, 6];
-$array4 = [16, 100];
-$array5 = [5, 11];
-$array = [[1, 2, 3, 4, 5], [7, 8, 9, 10, 11, 12], [3, 4, 5, 6]];
-[$begin1, $end1] = $array1;
-[$begin2, $end2] = $array2;
-[$begin3, $end3] = $array3;
-[$begin2, $end2] = $array2;
-[$begin2, $end2] = $array2;
-$new_array = array_merge([1, 2, 3, 4, 5], [7, 8, 9, 10, 11, 12], [3, 4, 5, 6]);
+$intervals = [
+    [1, 5],
+    [-10, 19],
+    [1, 7],
+    [16, 100],
+    [5, 11],
+    [-11, 100]
+];
+$newIntervals = [];
+$length = count($intervals);
+for ($i = 0; $i < $length; $i++) {
+    [$begin, $end] = $intervals[$i];
+    $newIntervals[] = range($begin, $end);
+}
 
-var_dump($new_array);
+$y = [1,3,-7, 4,100, 0];
+sort($y);
+var_dump($y);
+
 
--- sumIntervals.php
+++ sumIntervals.php
@@ -4,18 +4,33 @@ namespace Hexlet\Php\SumIntervals;
 
 function sumIntervals(array $intervals)
 {
+    $copyIntervals = $intervals;
+    sort($copyIntervals);
     $newIntervals = [];
-    $length = count($intervals);
+    $length = count($copyIntervals);
     for ($i = 0; $i < $length; $i++) {
-        [$begin, $end] = $intervals[$i];
-        $newIntervals[] = range($begin, $end);
+        [$begin, $end] = $copyIntervals[$i];
+        if ($begin !== $end) $newIntervals[] = range($begin, $end);
     }
-    for ($i = 0; $i < $length; $i++) {
-        if(array_intersect()
+    if (empty($newIntervals)) return 0;
+    $testInterval = $newIntervals[0];
+    $sum = 0;
+    $length = count($newIntervals);
+    for ($i = 1; $i < $length; $i++) {
+        if (array_intersect($testInterval, $newIntervals[$i])) {
+            $testInterval = array_unique(array_merge($testInterval, $newIntervals[$i]));
+        } else {
+            $sum +=  array_pop($testInterval) - array_shift($testInterval);
+            $testInterval = $newIntervals[$i];
+        }
     }
-    return array_merge($newIntervals);
+    $sum +=  array_pop($testInterval) - array_shift($testInterval);
+    return $sum;
 }
-$intervals = [[1,4], [7, 12], [3, 6]];
+$intervals = [
+    [1, 2],
+    [11, 12]
+];
 var_dump(sumIntervals($intervals));
 
 
