0f7ce139f797841af153bc851737cb521ff1b493
OlegSofonov <olesof@mail.ru>
2024-01-01 15:26:45 +0500

    add Map.php

--- main.php
+++ main.php
@@ -1,27 +1,9 @@
 <?php
 
-require_once 'getSortedNames.php';
+require_once 'Map.php';
 
-use function Hexlet\Php\getSortedNames;
+use function Hexlet\Php\make;
 
-$users = [
-    ['name' => 'Bronn', 'gender' => 'male', 'birthday' => '1973-03-23'],
-    ['name' => 'Reigar', 'gender' => 'male', 'birthday' => '1973-11-03'],
-    ['name' => 'Eiegon',  'gender' => 'male', 'birthday' => '1963-11-03'],
-    ['name' => 'Sansa', 'gender' => 'female', 'birthday' => '2012-11-03']
-];
+$map = make();
 
-var_dump(getSortedNames($users)); // ['Bronn', 'Eiegon', 'Reigar', 'Sansa']
-var_dump(getSortedNames([])); // []
-
-$users2 = [
-    ['name' => 'Jon', 'gender' => 'male', 'birthday' => '1980-11-03'],
-    ['name' => 'Robb','gender' => 'male', 'birthday' => '1980-05-14'],
-    ['name' => 'Tisha', 'gender' => 'female', 'birthday' => '2012-11-03'],
-    ['name' => 'Rick', 'gender' => 'male', 'birthday' => '2012-11-03'],
-    ['name' => 'Joffrey', 'gender' => 'male', 'birthday' => '1999-11-03'],
-    ['name' => 'Edd', 'gender' => 'male', 'birthday' => '1973-11-03']
-];
-
-// $this->assertEquals(['Edd', 'Joffrey', 'Jon', 'Rick', 'Robb', 'Tisha'], getSortedNames($users2));
-var_dump(getSortedNames($users2));
+var_dump($map);
