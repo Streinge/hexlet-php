933f93c14917bc4ee5bfc86f9f35f49b08da5c90
OlegSofonov <olesof@mail.ru>
2024-01-03 12:37:20 +0500

    update primer.php

--- primer.php
+++ primer.php
@@ -1,7 +1,12 @@
 <?php
 
-$arr = [];
-var_dump(empty($arr));
-$args = [3, 2];
-// Функция pow возводит первый аргумент в степень, переданную вторым аргументом
-var_dump(...$args);
+use function Funct\Collection\firstN;
+
+$users = [
+  ['name' => 'Tirion', 'birthday' => '1988-11-19'],
+  ['name' => 'Sam', 'birthday' => '1999-11-22'],
+  ['name' => 'Rob', 'birthday' => '1975-01-11'],
+  ['name' => 'Sansa', 'birthday' => '2001-03-20'],
+  ['name' => 'Tisha', 'birthday' => '1992-02-27']
+];
+var_dump(firstN($users));
