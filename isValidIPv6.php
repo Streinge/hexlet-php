f49a2a609b30141e2a0ba8c86d178faac500a53c
OlegSofonov <olesof@mail.ru>
2024-01-06 13:44:55 +0500

    update isValidIPv6.php

--- isValidIPv6.php
+++ isValidIPv6.php
@@ -47,12 +47,11 @@ function isCorrectNumberColons(string $lowerAddress): bool
     if (($numberThreeColons > 0 || $numberTwoColons > 1) || ($numberOneColon === 0 && $numberTwoColons === 0)) {
         return false;
     }
+    return true;
 }
+
 function isValidIPv6(string $address)
 {
-    if ($address === "::") {
-        return true;
-    }
     $lowerAddress = strtolower($address);
     $arrayAddress = explode(":", $lowerAddress);
     $arrayChars = flattenAll(array_map(fn($address) => str_split($address), $arrayAddress));
@@ -60,16 +59,13 @@ function isValidIPv6(string $address)
         isCorrectEdges($lowerAddress),
         isHexadecimalOrEmpty($arrayChars),
         isCorrectLength($arrayAddress),
-        isCorrectSize($arrayAddress)
+        isCorrectSize($arrayAddress),
+        isCorrectNumberColons($lowerAddress)
     ];
     if (in_array(false, $logicValues)) {
-        var_dump(isCorrectEdges($lowerAddress));
-        var_dump(isHexadecimalOrEmpty($arrayChars));
-        var_dump(isCorrectLength($arrayAddress));
-        var_dump(isCorrectSize($arrayAddress));
         return false;
     }
-    return $arrayChars;
+    return true;
 }
 
 //var_dump(isValidIPv6('2a02:0cb41:0:0:0:0:0:7')); // false
@@ -81,4 +77,19 @@ function isValidIPv6(string $address)
 //var_dump(isValidIPv6('e1b6:1daf9:6:0:c50:8c4:90e:e')); // false
 //var_dump(isValidIPv6('9f8:0:69S0:9:9:d9a:672:f90d')); // false
 //var_dump(isValidIPv6('2.001::')); // false
-var_dump(isValidIPv6('2001:::'));
+//var_dump(isValidIPv6('2001:::')); // false
+//var_dump(isValidIPv6('2001')); // false
+//var_dump(isValidIPv6('2001::0:64::2')); // false
+//var_dump(isValidIPv6('2607:G8B0:4010:801::1004')); //false
+//var_dump(isValidIPv6('2a03:2880:2130:cf05:face:b00c:0:1')); //true
+//var_dump(isValidIPv6('::b36:3c:f0:7:937')); //true
+//var_dump(isValidIPv6('000::B36:3C:00F0:7:937')); //true
+//var_dump(isValidIPv6('0B0:0F09:7f05:e2F3:0D:0:e0:7000')); //true
+//var_dump(isValidIPv6('10:d3:2d06:24:400c:5ee0:be:3d')); //true
+//var_dump(isValidIPv6('::')); //true
+//var_dump(isValidIPv6('::1')); //true
+//var_dump(isValidIPv6('1::1')); //true
+//var_dump(isValidIPv6('2a02:cb41:0:0:0:0:0:7')); //true
+//var_dump(isValidIPv6('e:6c::647:50:0:7')); //true
+//var_dump(isValidIPv6('1001:208:67:4f00:e3::2c6:0')); //true
+//var_dump(isValidIPv6('04:07A1:1404:0A0:A:080F:080:0')); //true
