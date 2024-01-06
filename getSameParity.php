e026ec07ceeeadf66158be898382733826fbf2f8
OlegSofonov <olesof@mail.ru>
2024-01-06 16:45:29 +0500

    add getSameParity.php

--- getSameParity.php
+++ getSameParity.php
@@ -1,27 +1,26 @@
 <?php
 
-namespace Hexlet\Php\GetSameParity;
+namespace Hexlet\Php;
 
-function getSameParity(array $items): array
+function getSameParity(array $data): array
 {
-    if (empty($items)) {
+    if ($data === []) {
         return [];
     }
-
-    $newItems = [];
-    $isEvenFirst = ($items[0] % 2 === 0);
-
-    foreach ($items as $item) {
-        $isEven = ($item % 2 === 0);
-        if ($isEven  === $isEvenFirst) {
-            $newItems[] = $item;
+    $parity = $data[0] % 2 === 0;
+    return array_values(array_filter($data, function ($item) use ($parity) {
+        if (($item % 2 === 0) === $parity) {
+            if ($item === 0) {
+                return true;
+            }
+            return $item;
         }
-    }
-
-    return $newItems;
+    }));
 }
 
-print_r(getSameParity([]));        // []
-print_r(getSameParity([1, 2, 3]));// [1, 3]
-print_r(getSameParity([1, 2, 8])); // [1]
-print_r(getSameParity([2, 2, 8])); // [2, 2, 8]
+//var_dump(getSameParity([])); // [])
+//var_dump(getSameParity([-1, 0, 1, -3, 10, -2])); // [-1, 1, -3]
+//var_dump(getSameParity([2, 0, 1, -3, 10, -2])); //[2, 0, 10, -2]
+//var_dump(getSameParity([2, 0, 10, -2])); // [2, 0, 10, -2]
+//var_dump(getSameParity([-5, 0, 1, -3, 10]));// [-5, 1, -3]
+var_dump(getSameParity([5, 0, 1, -3, 10])); // [5, 1, -3]
