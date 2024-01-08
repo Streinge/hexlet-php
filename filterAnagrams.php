f2fa0937d56a4d04782a711ec57697b03c5b9698
OlegSofonov <olesof@mail.ru>
2024-01-08 22:30:28 +0500

    update filterAnagrams.php

--- filterAnagrams.php
+++ filterAnagrams.php
@@ -2,17 +2,36 @@
 
 namespace Hexlet\Php;
 
-function filterAnagrams(string $word, array $list)
+/*function counter(string $word): array
 {
     $arrWord = str_split($word);
-    $numberChar = array_reduce($arrWord, function ($acc, $word) {
+    $countChar = array_reduce($arrWord, function ($acc, $word) { 
         $acc[$word] = ($acc[$word] ?? 0) + 1;
         return $acc;
     }, []);
-    //$result = array_filter($list, function ($testWord) use ($arrWord))
+    return $countChar;
+}
 
-    //$result = array
-    return $numberChar;
+function filterAnagrams(string $word, array $list)
+{
+    $countWord = counter($word);
+    $result = array_filter($list, function ($testWord) use ($countWord) {
+        var_dump($testWord);
+        $counterTest = counter($testWord);
+        return $counterTest === $countWord;
+    });
+
+    return $result;
+}*/
+function filterAnagrams(string $word, array $list)
+{
+    array_filter($list, function ($testWord) use ($word) {
+        $arrayWord = array_values(array_unique(str_split($testWord)));
+        return array_map(function ($char) use ($testWord, $word) {
+            return (substr_count($char, $testWord) === substr_count($char, $word));
+        }, $arrayWord);
+    });
+
 }
 
 var_dump(filterAnagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
