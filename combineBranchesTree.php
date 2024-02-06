4022ef413b9658bfec24bdcd781dd2e7bcdee05d
OlegSofonov <olesof@mail.ru>
2024-02-06 17:22:25 +0500

    finish combineBranchesTree.php

--- combineBranchesTree.php
+++ combineBranchesTree.php
@@ -15,101 +15,59 @@ function makeFlattenTree($tree, $flattenList, $parent = null)
     return array_reduce($branches, fn($iAcc, $child) => makeFlattenTree($child, $iAcc, $nameCity), $newAcc);
 }
 
+function changesConnectBranch($branchConnected, $rootBase)
+{
+    $key = array_key_first($branchConnected);
+    $parent = $branchConnected[$key][0];
+    $children = $branchConnected[$key][1] ?? [];
+    if ($key !== $rootBase) {
+        $newParent = array_shift($children);
+        array_shift($branchConnected);
+        if (!empty($parent)) {
+            $children[] = $parent;
+        }
+        $branchConnected[$key] = (!empty($children)) ? [$newParent, $children] : [$newParent];
+
+        return changesConnectBranch($branchConnected, $rootBase);
+    } else {
+        $newParent = null;
+        $children[] = $parent;
+        array_shift($branchConnected);
+        $branchConnected[$key] = (!empty($children)) ? [$newParent, $children] : [$newParent];
+        return array_reverse($branchConnected);
+    }
+}
+
 function buildsNewBranch($listBranchBase, $branchConnected)
 {
-
     $rootOld = array_key_first($branchConnected);
-    $newParent = $branchConnected[$rootOld][1][0];
-    
+    $rootNew = array_key_first($listBranchBase);
     $keysBase = array_keys($listBranchBase);
-
-
-
-    # меняю присоединяемую ветку
-    $modifiedBranchConnected = array_reduce(array_keys($branchConnected), function ($acc, $key) use ($branchConnected, $newParent, $keysBase) {
-
-        $parent = $branchConnected[$key][0];
-        $children = $branchConnected[$key][1] ?? [];
-        # формирую новых детей
-        $newKids = function ($children) use ($keysBase) {
-
-            $result = array_map(function ($child) use ($keysBase) {
-            # если детей из присоединяемой ветки нет в базовом, то
-            # их беру - других нет
-                if (!in_array($child, $keysBase)) {
-                    return $child;
-                }
-            }, $children);
-            $result = array_filter($result, fn($kid) => !is_null($kid));
-            sort($result);
-            return $result;
-        };
-        $kids = $newKids($children);
-
-        # обрабатываю корень присоединяемой ветки
-        if (empty($parent)) {
-
-            # устанавливаю ему родителя - новый корень и отфильтрованных детей
-            $acc[$key] = (empty($kids)) ? [$newParent] : [$newParent, $kids];
-
-            # если в присоединяемой ветке нет узла с корнем, как у базовой ветки
-            # то добавляю его в дети ему добавляю бывший корень, чтобы потом при обработке
-            # базовой ветки его можно было добавить в результат
-            if (empty($branchConnected[$newParent])) {
-                $acc[$newParent] = [null, [$key]];
+    if (!in_array($rootOld, $keysBase)) {
+        $modifiedBranchConnected = changesConnectBranch($branchConnected, $rootNew);
+    } else {
+        $modifiedBranchConnected = $branchConnected;
+    }
+    $matchingKeys = array_intersect(array_keys($listBranchBase), array_keys($modifiedBranchConnected));
+    $changedBaseBranch = array_reduce(
+        array_keys($listBranchBase),
+        function ($acc, $key) use ($listBranchBase, $modifiedBranchConnected, $matchingKeys) {
+            if (in_array($key, $matchingKeys)) {
+                $kidsBase = $listBranchBase[$key][1] ?? [];
+                $kidsConnect = $modifiedBranchConnected[$key][1] ?? [];
+                $diff = array_diff($kidsConnect, $kidsBase);
+                $kidsBase = [...$kidsBase, ...$diff];
+                sort($kidsBase);
+                $acc[$key] = !empty($kidsBase) ? [$listBranchBase[$key][0], $kidsBase] : [$listBranchBase[$key][0]];
             } else {
-                $childrenOther = $branchConnected[$newParent][1] ?? [];
-                # если этот узел есть, то просто в дети к ниму добавляю бывший корень.
-                $childrenOther[] = $key;
-
-                $otherKids = $newKids($childrenOther);
-
-                sort($otherKids);
-                $acc[$newParent] = ($otherKids === []) ? [$branchConnected[$newParent][0]] : [$branchConnected[$newParent][0], $otherKids];
+                $acc[$key] = $listBranchBase[$key];
             }
             return $acc;
-        } elseif (!empty($acc[$key])) {
-            return $acc;
-        }
-        $acc[$key] = (empty($kids)) ? [$branchConnected[$key][0]] : [$branchConnected[$key][0], $kids];
-        return $acc;
-    }, []);
-
-    
-    /*$modifiedBranchConnected = array_reduce(array_keys($modifiedBranchConnected), function ($acc, $key) use ($listBranchBase, $modifiedBranchConnected) {
-        $parent = ((array_key_exists($key, $listBranchBase)) ? $key : array_key_first($listBranchBase));
-
-        $acc[$key] = empty($modifiedBranchConnected[$key][1]) ? [$parent] : [$parent, $modifiedBranchConnected[$key][1]];
-        return $acc;
-    }, []);*/
-
-
-    $matchingKeys = array_intersect(array_keys($listBranchBase), array_keys($modifiedBranchConnected));
-
-    $changedBaseBranch = array_reduce(array_keys($listBranchBase), function ($acc, $key) use ($listBranchBase, $modifiedBranchConnected, $matchingKeys) {
-        if (in_array($key, $matchingKeys)) {
-
-            $kidsBase = $listBranchBase[$key][1] ?? [];
-            
-            $kidsConnect = $modifiedBranchConnected[$key][1] ?? [];
-
-            $diff = array_diff($kidsConnect, $kidsBase);
-            
-            $kidsBase = [...$kidsBase, ...$diff];
-
-            sort($kidsBase);
-
-            $acc[$key] = !empty($kidsBase) ? [$listBranchBase[$key][0], $kidsBase] : [$listBranchBase[$key][0]];
-        } else {
-            $acc[$key] = $listBranchBase[$key];
-        }
-        return $acc;
-    }, []);
-
+        },
+        []
+    );
     $diffNodes = array_diff_key($modifiedBranchConnected, $changedBaseBranch);
-    
     $result = [...$changedBaseBranch, ...$diffNodes,];
-
     return $result;
 }
 
@@ -123,25 +81,19 @@ function reverseConverted($children, $list)
         $acc[] = ($newChildren === []) ? [$child] : [$child, reverseConverted($newChildren, $list)];
         return $acc;
     }, []);
-
     return $result;
 }
 
 function combine($branchBase, ...$branches)
 {
-
     $listBranchBase = makeFlattenTree($branchBase, [], null);
     foreach ($branches as $branch) {
         $listBranch = makeFlattenTree($branch, [], null);
         $listBranchBase = buildsNewBranch($listBranchBase, $listBranch);
     }
-    //$listBranch = makeFlattenTree($branches, [], null);
-    //$listNewBranch = buildsNewBranch($listBranchBase, $listBranch);
-
     $root = array_key_first($listBranchBase);
     $children = $listBranchBase[$root][1];
     $new = [$root, reverseConverted($children, $listBranchBase)];
-    
     return $new;
 }
 
@@ -225,14 +177,5 @@ $expected4 = ['B', [
 var_dump(combine($branch1, $branch2, $branch3) === $expected1);
 var_dump(combine($branch2, $branch1, $branch3) === $expected2);
 var_dump(combine($branch3, $branch2, $branch1) === $expected3);
-var_dump(combine($branch3, $branch2, $branch1) === $expected4);
-//print_r(combine($branch2, $branch3));
-/*$actual1 = combine($branch1, $branch2, $branch3);
-$actual2 = combine($branch2, $branch1, $branch3);
-$actual3 = combine($branch3, $branch2, $branch1);
-$actual4 = combine($branch2, $branch3);
-
-$this->assertEquals($expected1, sortTree($actual1));
-$this->assertEquals($expected2, sortTree($actual2));
-$this->assertEquals($expected3, sortTree($actual3));
-$this->assertEquals($expected4, sortTree($actual4));*/
+var_dump(combine($branch2, $branch3) === $expected4);
+//print_r(combine($branch2, $branch1, $branch3));
