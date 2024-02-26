2af0269a503b9b84812c08769e7633f0ac695a07
OlegSofonov <olesof@mail.ru>
2024-02-26 18:31:30 +0500

    stert OOP

---
+++ circle.php
@@ -0,0 +1,11 @@
+<?php
+
+//require_once __DIR__ . '/vendor/autoload.php';
+
+namespace Hexlet\Php;
+
+class Circle
+{
+    public $center;
+    public $radius;
+}
---
+++ point.php
@@ -0,0 +1,11 @@
+<?php
+
+//require_once __DIR__ . '/vendor/autoload.php';
+
+namespace Hexlet\Php;
+
+class Point
+{
+    public $x;
+    public $y;
+}
